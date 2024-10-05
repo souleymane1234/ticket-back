45e29ae9b4c88e4507d36034e503931e1d1f6be3
oladigitalservices <oladigitalservices@gmail.com>
2024-10-05 17:21:40 +0000

    new update

--- server.js
+++ server.js
@@ -1,78 +1,76 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const PORT = 5000;
-const {mogoUrl} = require('./keys');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const PORT = 5000;
+const { mogoUrl } = require("./keys");
 const qrcode = require("qrcode");
-const cors = require('cors');
-const fs = require('fs');
+const cors = require("cors");
+const fs = require("fs");
+const dotenv = require("dotenv");
 
-require('./models/User')
-require('./models/Event')
-require('./models/Ticket')
-require('./models/UserTicket')
-require('./models/onePage')
-require('./models/CompagnieTransport')
-require('./models/TicketTransport')
-require('./models/Sport')
-require('./models/Cinema')
-require('./models/Img')
-const requireToken = require('./middleware/requireToken')
-const AuthRoutes = require('./routes/AuthRoutes')
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+require("./models/User");
+require("./models/Event");
+require("./models/Ticket");
+require("./models/UserTicket");
+require("./models/onePage");
+require("./models/CompagnieTransport");
+require("./models/TicketTransport");
+require("./models/Sport");
+require("./models/Cinema");
+require("./models/Img");
+const requireToken = require("./middleware/requireToken");
+const AuthRoutes = require("./routes/AuthRoutes");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000"
-// }));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
- 
-// Set EJS as templating engine
+
+app.use(bodyParser.json());
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("Database connected successful.");
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(AuthRoutes)
-app.use('/uploads', express.static('uploads'));
+app.use(AuthRoutes);
+app.use("/uploads", express.static("uploads"));
 
-const uri = "mongodb+srv://TicketAgenceImage:Tm8cTxvt1E3CHScS@cluster0.ihbx5zc.mongodb.net/?retryWrites=true&w=majority";
+// const uri =
+//   "mongodb+srv://TicketAgenceImage:Tm8cTxvt1E3CHScS@cluster0.ihbx5zc.mongodb.net/?retryWrites=true&w=majority";
 
-
-// mongoose.connect(mogoUrl)
-mongoose.connect(uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
-// .then(values=>console.log('connected to mongo yeah', values)).catch((error=>console.log('this isss error', error)));
-
-mongoose.connection.on('connected', () => {
-    console.log('connected to mongo yeah')
-})
-
-mongoose.connection.on('error', (err) => {
-    console.log('this id error', err)
-})
-
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-// app.all('*', (req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     next();
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
 
+// mongoose.connection.on("connected", () => {
+//   console.log("connected to mongo yeah");
+// });
 
-app.post('/', (req,res) => {
-    console.log(req.body)
-    res.send('hello')
-})
-app.get('/',requireToken, (req,res) => {
-  res.send("Your mail is" + req.user.email)
-})
+// mongoose.connection.on("error", (err) => {
+//   console.log("this id error", err);
+// });
 
-app.listen(PORT, () => {
-     console.log("server running" + PORT)
-})
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("hello");
+});
+app.get("/", requireToken, (req, res) => {
+  res.send("Your mail is" + req.user.email);
+});
+
+// app.listen(PORT, () => {
+//   console.log("server running" + PORT);
+// });
